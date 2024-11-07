c465e33c189ba9100d2ee5eccc2def4bf1416788
AiYuri777 <rubyzhao0815@gmail.com>
2024-11-07 00:14:15 -0500

    Codes for use_case.delete_ingredient

--- src/main/java/use_case/delete_ingredient/DeleteIngredientInteractor.java
+++ src/main/java/use_case/delete_ingredient/DeleteIngredientInteractor.java
@@ -1,4 +1,32 @@
 package use_case.delete_ingredient;
 
-public class DeleteIngredientInteractor {
+
+import entity.Ingredient;
+
+import java.util.List;
+
+/**
+ * The Delete Ingredient Interactor.
+ */
+public class DeleteIngredientInteractor implements DeleteIngredientInputBoundary {
+    private final DeleteIngredientUserDataAccessInterface userDataAccessObject;
+    private final DeleteIngredientOutputBoundary deleteIngredientPresenter;
+
+    public DeleteIngredientInteractor(DeleteIngredientUserDataAccessInterface userDataAccessInterface,
+                                      DeleteIngredientOutputBoundary deleteIngredientOutputBoundary) {
+        this.userDataAccessObject = userDataAccessInterface;
+        this.deleteIngredientPresenter = deleteIngredientOutputBoundary;
+    }
+
+    @Override
+    public void execute(DeleteIngredientInputData deleteIngredientInputData) {
+        final List<Ingredient> ingredients = deleteIngredientInputData.getIngredients();
+        final Ingredient ingredient = deleteIngredientInputData.getIngredient();
+        ingredients.remove(ingredient);
+        userDataAccessObject.setCurrentIngredients(ingredients);
+        final DeleteIngredientOutputData deleteIngredientOutputData = new DeleteIngredientOutputData(ingredients,
+                ingredient, false);
+        // * tell the presenter to prepare a success view.
+        deleteIngredientPresenter.prepareSuccessView(deleteIngredientOutputData);
+    }
 }
--- src/main/java/use_case/delete_ingredient/DeleteIngredientOutputBoundary.java
+++ src/main/java/use_case/delete_ingredient/DeleteIngredientOutputBoundary.java
@@ -1,4 +1,18 @@
 package use_case.delete_ingredient;
 
+/**
+ * The output boundary for the Delete Ingredient Use Case.
+ */
 public interface DeleteIngredientOutputBoundary {
+    /**
+     * Prepares the success view for the Delete Ingredient Use Case.
+     * @param outputData the output data
+     */
+    void prepareSuccessView(DeleteIngredientOutputData outputData);
+
+    /**
+     * Prepares the failure view for the Delete Ingredient Use Case.
+     * @param errorMessage the explanation of the failure
+     */
+    void prepareFailView(String errorMessage);
 }
