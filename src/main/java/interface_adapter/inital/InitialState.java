1b67f751413a027581c0ba46bbb28bb6cf8f12f7
AiYuri777 <rubyzhao0815@gmail.com>
2024-11-16 16:10:09 -0500

    interface_adapter.initial.InitialState

--- src/main/java/interface_adapter/inital/InitialState.java
+++ src/main/java/interface_adapter/inital/InitialState.java
@@ -1,4 +1,17 @@
 package interface_adapter.inital;
 
+import entity.Ingredient;
+
+import java.util.List;
+
 public class InitialState {
+    private List<Ingredient> ingredients;
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
 }
