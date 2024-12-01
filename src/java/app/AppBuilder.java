010256be89bda792821a3b4d76e8ba3ceb81728c
oldeight888 <taoted7@gmail.com>
2024-11-30 20:46:32 -0500

    modification in AppBuilder

--- src/java/app/AppBuilder.java
+++ src/java/app/AppBuilder.java
@@ -231,4 +231,14 @@ public class AppBuilder {
         return application;
     }
 
+    public AppBuilder addExpirartionWarningUseCase() {
+        final ExpiredIngredientOutputBoundary expiredIngredientOutputBoundary = new ExpirationWarningPresenter(
+                expirationWarningViewModel, initialViewModel, viewManagerModel);
+        final ExpiredIngredientInteractor interactor = new ExpiredIngredientInteractor(expiredIngredientOutputBoundary);
+        final ExpirationWarningController controller = new ExpirationWarningController(interactor);
+        if (expirationWarningView != null) {
+            expirationWarningView.setExpirationWarningController(controller);
+        }
+        return this;
+    }
 }
