21ccc080dac11f89f9b86d70812ae6347cad4a70
TTAT413 <taoted7@gmail.com>
2024-12-03 17:17:49 -0500

    Updating the code again

--- src/java/interface_adapter/ExpirationWarning/ExpirationWarningPresenter.java
+++ src/java/interface_adapter/ExpirationWarning/ExpirationWarningPresenter.java
@@ -8,11 +8,24 @@ import use_case.expired_food.ExpiredIngredientOutputBoundary;
 
 import java.util.List;
 
+/**
+ * The ExpirationWarningPresenter class implements the ExpiredIngredientOutputBoundary
+ * interface, facilitating the presentation of expiration warning data and managing
+ * transitions between views.
+ */
 public class ExpirationWarningPresenter implements ExpiredIngredientOutputBoundary {
     private final ExpirationWarningViewModel viewModel;
     private final InitialViewModel initialViewModel;
     private final ViewManagerModel viewManagerModel;
 
+    /**
+     * Constructs an ExpirationWarningPresenter with the specified ViewModel, InitialViewModel,
+     * and ViewManagerModel.
+     *
+     * @param viewModel       the ViewModel for the expiration warning view
+     * @param initialViewModel the ViewModel for the initial view
+     * @param viewManagerModel the model for managing view states
+     */
     public ExpirationWarningPresenter(ExpirationWarningViewModel viewModel,
                                       InitialViewModel initialViewModel,
                                       ViewManagerModel viewManagerModel) {
@@ -21,12 +34,22 @@ public class ExpirationWarningPresenter implements ExpiredIngredientOutputBounda
         this.viewManagerModel = viewManagerModel;
     }
 
+    /**
+     * Retrieves the list of current ingredients.
+     *
+     * @return a list of current ingredients
+     */
     @Override
     public List<Ingredient> getCurrentIngredients(){
         final InitialState initialState = initialViewModel.getState();
         return initialState.getIngredients();
     }
 
+    /**
+     * Deletes the specified ingredient from the system.
+     *
+     * @param ingredient the ingredient to delete
+     */
     @Override
     public void deleteIngredient(Ingredient ingredient){
         final InitialState initialState = initialViewModel.getState();
@@ -35,6 +58,10 @@ public class ExpirationWarningPresenter implements ExpiredIngredientOutputBounda
         initialViewModel.firePropertyChanged();
     }
 
+
+    /**
+     * Switches the view back to the initial screen.
+     */
     @Override
     public void switchToInitialView() {
         viewManagerModel.setState(initialViewModel.getViewName());
