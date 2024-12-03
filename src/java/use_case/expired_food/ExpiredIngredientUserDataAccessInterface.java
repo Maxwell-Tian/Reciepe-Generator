fc9da430c7d00eaf2a744436146af23d986d5adb
TTAT413 <taoted7@gmail.com>
2024-12-03 17:14:14 -0500

    Updating the code

--- src/java/use_case/expired_food/ExpiredIngredientUserDataAccessInterface.java
+++ src/java/use_case/expired_food/ExpiredIngredientUserDataAccessInterface.java
@@ -5,9 +5,25 @@ import entity.Ingredient;
 import java.io.FileNotFoundException;
 import java.util.List;
 
+/**
+ * The ExpiredIngredientUserDataAccessInterface defines the operations for
+ * accessing and managing ingredient data in the system.
+ */
 public interface ExpiredIngredientUserDataAccessInterface {
 
+    /**
+     * Retrieves the list of current ingredients.
+     *
+     * @return a list of current ingredients
+     * @throws FileNotFoundException if the ingredient data cannot be found
+     */
     List<Ingredient> getCurrentIngredients() throws FileNotFoundException;
 //    boolean existsByIngredientName(String ingredientName);
+
+    /**
+     * Deletes the specified ingredient from the system.
+     *
+     * @param ingredient the ingredient to delete
+     */
     void deleteIngredient(Ingredient ingredient);
 }
